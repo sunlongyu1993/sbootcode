b7c582dc4e3c736278c446408c42ddc3de78e5f6
sunlongyu <11Wn98f1927sly@@>
2021-04-27 11:22:59 +0800

    第一个demo

---
+++ springboot-firstdemo/src/main/java/cn/springboot/study01/FirstSpringApplication.java
@@ -0,0 +1,16 @@
+package cn.springboot.study01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 孙珑瑜
+ * @version 20210427
+ * springboot 的启动类
+ */
+@SpringBootApplication
+public class FirstSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringApplication.class,args);
+    }
+}
