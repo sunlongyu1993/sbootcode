d8aebd7e723bc617f84e64e38d43a8a30e85f5a8
sunlongyu <11Wn98f1927sly@@>
2021-05-18 22:17:38 +0800

    config配置文件学习

---
+++ springboot-config/src/main/java/cn/springboot/study01/TestController.java
@@ -0,0 +1,18 @@
+package cn.springboot.study01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 孙珑瑜
+ * @version 20210427
+ *
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello,springboot_config!!!!";
+    }
+}
